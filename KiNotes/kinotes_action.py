f155e5f30283aaadd63d9c31342a4953da912ab6
Pramil W <pramil.wakchaure@gmail.com>
2025-11-26 13:07:35 +0530

    plugin appears

--- KiNotes/kinotes_action.py
+++ KiNotes/kinotes_action.py
@@ -1,26 +1,19 @@
 import pcbnew
 import wx
+import os
 
 class KiNotesFrame(wx.Frame):
     def __init__(self, parent=None, title="KiNotes - PCB Notes"):
         super(KiNotesFrame, self).__init__(parent, title=title, size=(400, 500))
-
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
-
-        # Temporary text area (Markdown later)
         self.text_ctrl = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_RICH2)
-
-        # Subtle branding footer
         footer = wx.StaticText(panel, label="Powered by PCBtools.xyz")
         footer.SetForegroundColour(wx.Colour(100, 100, 100))
-
         vbox.Add(self.text_ctrl, 1, wx.EXPAND | wx.ALL, 5)
         vbox.Add(footer, 0, wx.ALIGN_CENTER | wx.BOTTOM, 5)
-
         panel.SetSizer(vbox)
         self.Layout()
-
 
 class KiNotesActionPlugin(pcbnew.ActionPlugin):
     def defaults(self):
@@ -28,12 +21,10 @@ class KiNotesActionPlugin(pcbnew.ActionPlugin):
         self.category = "Utilities"
         self.description = "Smart PCB Notes linked to your design"
         self.show_toolbar_button = True
-        self.icon_file_name = self.get_icon()
-
-    def get_icon(self):
-        import os
-        return os.path.join(os.path.dirname(__file__), "resources", "icon.png")
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), "resources", "icon.png")
 
     def Run(self):
         frame = KiNotesFrame(parent=None)
         frame.Show(True)
+
+KiNotesActionPlugin().register()
